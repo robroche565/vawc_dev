c3800107e22eeb33bfd01c4a2f85b7eb5a551cd1
robroche565 <111483459+robroche565@users.noreply.github.com>
2024-02-26 17:45:37 +0800

    aded admin

--- vawc/vawc/settings.py
+++ vawc/vawc/settings.py
@@ -105,8 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 DEFAULT_FROM_EMAIL = 'vawcdilg@gmail.com'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
